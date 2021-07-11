32334b9852dc72c7d760d4fb6d171ac1e84b5c47
tsukaryu <drgon8otsuka@gmail.com>
2021-07-12 00:38:42 +0900

    add google font and styles

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,20 @@
+import { Fragment } from 'react';
+
 export default function Home() {
-  return <div>BIRD MAN</div>;
+  return (
+    <Fragment>
+      <section className='title'>
+        <div>title</div>
+      </section>
+      <section className='one'>
+        <div>one</div>
+      </section>
+      <section className='two'>
+        <div>two</div>
+      </section>
+      <section className='three'>
+        <div>three</div>
+      </section>
+    </Fragment>
+  );
 }
