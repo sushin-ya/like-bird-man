b939d7cb55fdd49086aa2325ee6b16c2ca5838a8
tsukaryu <drgon8otsuka@gmail.com>
2021-07-12 01:55:16 +0900

    delete gsap

--- components/One.js
+++ components/One.js
@@ -1,18 +1,9 @@
-import { useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
-
 export default function One() {
-  let line = useRef(null);
-  useEffect(() => {
-    gsap.fromTo(line, { opacity: 0 }, { opacity: 1, duration: 1 });
-  }, [line]);
   return (
     <section className='one'>
       <div className='text'>
-        <div ref={(el) => (line = el)} className='line'>
-          IT COMES DOWN TO THE RAIN, THE SUNLIGHT, THE TRAFFIC, THE NIGHTS AND
-          THE DAYS OF THE YEARS, THE FACES.
-        </div>
+        IT COMES DOWN TO THE RAIN, THE SUNLIGHT, THE TRAFFIC, THE NIGHTS AND THE
+        DAYS OF THE YEARS, THE FACES.
       </div>
     </section>
   );
