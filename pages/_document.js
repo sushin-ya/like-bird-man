8473ce53c3dc1fccb90bf45fd257b637ab859b6d
tsukaryu <drgon8otsuka@gmail.com>
2021-07-12 13:50:53 +0900

    add media query

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,11 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Teko:wght@700&display=swap'
             rel='stylesheet'
           />
+          <title>AS THE POEMS GO</title>
+          <meta
+            name='viewport'
+            content='initial-scale=1.0, width=device-width'
+          />
         </Head>
         <body>
           <Main />
